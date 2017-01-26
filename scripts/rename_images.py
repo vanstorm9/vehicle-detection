a0cd3a882ae0a3fb04cff0d910519140101eeb95
vanstorm9 <antlowhur@yahoo.com>
2017-01-26 17:49:00 -0500

    Initalizing dataset

---
+++ scripts/rename_images.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import os
+from time import time
+
+starting_value = 5766
+
+root_path = '../dataset/GTI/'
+path_to_save = '../dataset/no-vehicle/'
+
+slash = '/'
+root = os.listdir(root_path)
+
+print 'Iterating through folders:'
+
+t0 = time()
+
+
+# Iterating through the item directories to get dir
+for files in root:
+
+
+	# To try to check if image
+	try:
+		img = Image.open(root_path + files)
+	except IOError:
+		continue
+
+	width = img.size[0]
+	height = img.size[1]
+
+	
+	img2 = img.resize((width, height), Image.ANTIALIAS)
+
+	removeStr = root_path + files
+	saveStr = path_to_save + str(starting_value) + '.png'
+
+	os.remove(removeStr)
+	img2.save(saveStr,"PNG")
+	
+	starting_value = starting_value + 1
+
+total_time = time() - t0
+print 'Counted ', str(starting_value),' files'
+print 'Resize time: ', total_time, 's'
